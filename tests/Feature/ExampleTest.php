52d2e79aaf45a9438ce991c7743cbe8ce2ddce8c
Mo Khosh <mskhoshnazar@gmail.com>
2024-08-13 14:49:27 +0330

    add failing test for authenticated getUrl bug

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Models\User;
+use Filament\Facades\Filament;
+
+use function Pest\Laravel\be;
+
 it('returns proper login links', function () {
     $roles = ['admin', 'editor', 'user'];
 
     foreach ($roles as $role) {
-        $urls[] = \Filament\Facades\Filament::getPanel($role)->getUrl();
+        $urls[] = Filament::getPanel($role)->getUrl();
     }
 
     expect($urls)
@@ -12,5 +17,31 @@ it('returns proper login links', function () {
             "http://filament-geturl-bug.test/admin/login",
             "http://filament-geturl-bug.test/editor/login",
             "http://filament-geturl-bug.test/user/login",
+        ]);
+});
+
+it('fails returning proper login links when authenticated', function () {
+    be(User::factory()->create());
+    $roles = ['admin', 'editor', 'user'];
+
+    foreach ($roles as $role) {
+        $urls[] = Filament::getPanel($role)->getUrl();
+    }
+
+    foreach ($roles as $role) {
+        $urls2[] = Filament::getPanel($role)->getUrl();
+    }
+
+    expect($urls)
+        ->toBe([
+            "http://filament-geturl-bug.test/admin",
+            "http://filament-geturl-bug.test/editor",
+            "http://filament-geturl-bug.test/user",
+        ])
+        ->and($urls2)
+        ->toBe([
+            "http://filament-geturl-bug.test/admin",
+            "http://filament-geturl-bug.test/editor",
+            "http://filament-geturl-bug.test/user",
         ]);
 });
